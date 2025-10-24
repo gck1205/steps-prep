a3788cbd9d72c281dee3a5f8fc8fc8d6ac69e66c
gck1205 <111493644+gck1205@users.noreply.github.com>
2025-10-24 08:02:40 +0530

    p5

---
+++ cloudrun.py
@@ -0,0 +1,19 @@
+CloudRun
+
+Services
+
+Job
+#############
+Build CloudRun objects using following: 
+
+1) Docker & Image  - Specify the docker registry details along with image name and tag
+
+2) Github - passing repo details..Cloud run generates image and deploys to cloudrun
+
+3) Function - new functionality added recently
+
+##Create 2 files
+
+1) Dockerfile 
+2) main.py --> Extracts data and pushes to cloud storage
+
