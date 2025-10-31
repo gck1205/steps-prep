252cb33c8364b061ee599bd91d53c7fc4427a0b0
gck1205 <111493644+gck1205@users.noreply.github.com>
2025-10-31 21:42:09 +0530

    p31-2

--- read_yaml.py
+++ read_yaml.py
@@ -3,4 +3,4 @@ import yaml
 if __name__ == "__main__":
     with open(r'C:\Users\chaitanya.garigipati\OneDrive - Accenture\Documents\steps-prep\data_files\bio.yaml', 'r') as file:
         data = yaml.safe_load(file)
-        print(data)
+        print(data['projects'][1]['technologies'][1])
